2e35d184b76b5deb2224616c3a61d212c58256ba
Sergey Petrin <spetrin@gmail.com>
2023-10-23 22:56:23 +0300

    Update state colors

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+const previousStateContainer = document.getElementById('previous-state');
 const previousStateName = document.getElementById('previous-state-name');
 const previousPeriods = document.getElementById('previous-state-periods');
+
+const currentStateContainer = document.getElementById('current-state');
 const currentStateName = document.getElementById('current-state-name');
 const currentPeriods = document.getElementById('current-state-periods');
+
+const nextStateContainer = document.getElementById('next-state');
 const nextStateName = document.getElementById('next-state-name');
 const nextPeriods = document.getElementById('next-state-periods');
 
@@ -16,6 +21,7 @@ class States {
             'statePeriods': '' +
                 '<div class="current-state-period">07.00 - 10.00</div>' +
                 '<div class="current-state-period">17.00 - 21.00</div>',
+            'stateColor': '#f2c76e',
         }
     }
 
@@ -24,14 +30,16 @@ class States {
             'stateName': 'Полупик',
             'statePeriods': '' +
                 '<div class="current-state-period">10.00 - 17.00</div>' +
-                '<div class="current-state-period">21.00 - 23.00</div>'
+                '<div class="current-state-period">21.00 - 23.00</div>',
+            'stateColor': '#ebe8e7',
         }
     }
 
     getNight() {
         return {
             'stateName': 'Ночь',
-            'statePeriods': '<div class="previous-state-period">23.00 - 07.00</div>'
+            'statePeriods': '<div class="previous-state-period">23.00 - 07.00</div>',
+            'stateColor': '#bd9dea',
         }
     }
 }
@@ -65,11 +73,14 @@ if (7 <= currHour && currHour < 10) {
 }
 
 
+previousStateContainer.style.backgroundColor = previousState['stateColor'];
 previousStateName.innerText = previousState['stateName'];
 previousPeriods.innerHTML = previousState['statePeriods'];
 
+currentStateContainer.style.backgroundColor = currentState['stateColor'];
 currentStateName.innerText = currentState['stateName'];
 currentPeriods.innerHTML = currentState['statePeriods'];
 
+nextStateContainer.style.backgroundColor = nextState['stateColor'];
 nextStateName.innerText = nextState['stateName'];
 nextPeriods.innerHTML = nextState['statePeriods'];
